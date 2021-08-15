a12d7719160d8883e3a7ff17ec550edb1e0f60b6
Aman Singh <amanksofficial@gmail.com>
2021-08-15 19:34:29 +0530

    file upload added

--- node-backend/routes/categories.js
+++ node-backend/routes/categories.js
@@ -1,11 +1,22 @@
 const router = require("express").Router();
 const Category = require("../models/Category");
 
+// ADD NEW CATEGORY
 router.post("/", async (req, res) => {
     const newCategory = new Category(req.body);
     try {
         const savedCategory = await newCategory.save();
         res.status(200).json(savedCat);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET ALL CATEGORIES
+router.get("/", async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.status(200).json(categories);
     } catch (err) {
         res.status(500).json(err);
     }
--- node-backend/server.js
+++ node-backend/server.js
@@ -4,7 +4,9 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
-const postRoutes = require("./routes/posts")
+const postRoutes = require("./routes/posts");
+const catogaryRoutes = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 
@@ -19,8 +21,24 @@ mongoose
     .then(console.log("connected to mongoDB"))
     .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+    destination: (req, file, fn) => {
+        fn(null, "images");
+    },
+    filename: (req, file, fn) => {
+        fn(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => console.log("server started"));
